d4e4a6fe0d95a4a4e5923950924a291a88d41333
tuannt <tuannt@greenglobal.vn>
2020-10-15 10:06:51 +0700

    feat(System): create migration, add variable config
    - Variable: STORE_CONFIG with value 'database' or 'file', default 'database'
    
    see #1

---
+++ src/Database/Migrations/2020_10_14_090647_create_laravel_config_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLaravelConfigTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        if (env('STORE_CONFIG', 'database') === 'database') {
+            Schema::create('laravel_config', function (Blueprint $table) {
+                $table->id();
+                $table->string('code');
+                $table->string('value');
+                $table->string('dèault')->nullable();
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('laravel_config');
+    }
+}
