a92b42d256e80b95ce4ffb37b452c1ad9df5bf8f
HUNG HUYNH BA MANH <hunghbm@greenglobal.vn>
2021-02-05 13:36:57 +0700

    Add new decryption script

---
+++ decrypt_secret.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $HOME/secrets/laravel-config-firebase.json laravel-config-firebase.json.gpg
